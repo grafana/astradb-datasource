82c9b048125064ac067cbd5c058f0dbba34df88d
Taewoo K <taewoo.kim@grafana.com>
2022-08-30 10:08:04 -0400

    Template variable and cql editor (#14)
    * add template variable, query type variable, cql editor
    
    * add a file for query editor
    
    * cleanup - delete comment, add lines at the EOF
    
    * format

--- src/components/QueryEditor.tsx
+++ src/components/QueryEditor.tsx
@@ -1,15 +1,14 @@
 import React, { useCallback } from 'react';
 import { QueryEditorProps } from '@grafana/data';
 import { DataSource } from '../datasource';
-import { AstraQuery, AstraSettings, AutoSizerProps, Format } from '../types';
+import { AstraQuery, AstraSettings, AutoSizerProps } from '../types';
 import { CQLEditor } from './CQLEditor';
 // @ts-ignore
 import AutoSizer from 'react-virtualized-auto-sizer';
-import { css } from '@emotion/css';
 
 type Props = QueryEditorProps<DataSource, AstraQuery, AstraSettings>;
 
-export const QueryEditor = ({ query, onChange, onRunQuery }: Props) => {
+export const QueryEditor = ({ query, datasource, onChange, onRunQuery }: Props) => {
   const processQuery = useCallback(
     (q: AstraQuery) => {
       if (isQueryValid(q) && onRunQuery) {
@@ -26,38 +25,14 @@ export const QueryEditor = ({ query, onChange, onRunQuery }: Props) => {
     }
   };
 
-  const onSqlChange = (sql: string) => {
-    if (sql.trim() !== '') {
-      const format = sql.toLowerCase().includes('as time') ? Format.TIMESERIES : Format.TABLE;
-      onChange({ ...query, rawCql: sql, format });
-      onRunQuery();
-    }
-  };
-
-  const run = () => onSqlChange(query.rawCql || '');
-
-  const styles = {
-    wrapper: css`
-      position: relative;
-    `,
-    run: css`
-      position: absolute;
-      top: 2px;
-      left: 6px;
-      z-index: 100;
-      color: green;
-    `,
-  };
-
   return (
-    <div style={{ width: '100%', height: '300px' }} className={styles.wrapper}>
-      <a onClick={run} className={styles.run}>
-        <i className="fa fa-play"></i>
-      </a>
+    <div style={{ width: '100%', height: '300px' }} >
       <AutoSizer defaultHeight="300px" defaultWidth="100%">
         {(props: AutoSizerProps) => (
           <CQLEditor
             query={query}
+            datasource={datasource}
+            onRunQuery={onRunQuery}
             onChange={onQueryChange}
             width={props.width}
             height={props.height}
--- src/datasource.ts
+++ src/datasource.ts
@@ -1,9 +1,56 @@
-import { DataSourceInstanceSettings } from '@grafana/data';
-import { DataSourceWithBackend } from '@grafana/runtime';
+import { DataFrame, DataQueryRequest, DataQueryResponse, DataSourceInstanceSettings, ScopedVars, vectorator } from '@grafana/data';
+import { DataSourceWithBackend, getTemplateSrv } from '@grafana/runtime';
+import { uniqueId } from 'lodash';
 import { AstraQuery, AstraSettings } from './types';
 
 export class DataSource extends DataSourceWithBackend<AstraQuery, AstraSettings> {
   constructor(instanceSettings: DataSourceInstanceSettings<AstraSettings>) {
     super(instanceSettings);
+  }
+
+  applyTemplateVariables(query: AstraQuery, scopedVars: ScopedVars) {
+    const sql = this.replace(query.rawCql || '', scopedVars) || '';
+    return { ...query, rawCql: sql };
+  }
+
+  replace(value?: string, scopedVars?: ScopedVars) {
+    if (value !== undefined) {
+      return getTemplateSrv().replace(value, scopedVars, this.format);
+    }
+    return value;
+  }
+
+  format(value: any) {
+    if (Array.isArray(value)) {
+      return `'${value.join("','")}'`;
+    }
+    return value;
+  }
+
+  async metricFindQuery(query: AstraQuery) {
+    if (!query.rawCql) {
+      return [];
+    }
+    const frame = await this.runQuery(query);
+    if (frame.fields?.length === 0) {
+      return [];
+    }
+    if (frame?.fields?.length === 1) {
+      return vectorator(frame?.fields[0]?.values).map((text) => ({ text, value: text }));
+    }
+    // convention - assume the first field is an id field
+    const ids = frame?.fields[0]?.values;
+    return vectorator(frame?.fields[1]?.values).map((text, i) => ({ text, value: ids.get(i) }));
+  }
+
+  runQuery(request: Partial<AstraQuery>): Promise<DataFrame> {
+    return new Promise((resolve) => {
+      const req = {
+        targets: [{ ...request, refId: uniqueId() }],
+      } as DataQueryRequest<AstraQuery>;
+      this.query(req).subscribe((res: DataQueryResponse) => {
+        resolve(res.data[0] || { fields: [] });
+      });
+    });
   }
 }
