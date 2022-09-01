0ff1d447e8b40d12296e2ac2afaf9ff29a50e12e
Sriramajeyam Sugumaran <yesoreyeram@gmail.com>
2022-09-01 09:47:58 +0100

    Test framer

--- go.mod
+++ go.mod
@@ -4,30 +4,53 @@ go 1.18
 
 require (
 	github.com/araddon/dateparse v0.0.0-20210429162001-6b43995a97de
+	github.com/docker/go-connections v0.4.0
 	github.com/grafana/grafana-plugin-sdk-go v0.139.0
 	github.com/stargate/stargate-grpc-go-client v0.0.0-20220822130422-9a1c6261d4fa
 	github.com/stretchr/testify v1.7.2
+	github.com/testcontainers/testcontainers-go v0.13.0
 	google.golang.org/grpc v1.41.0
 )
 
 require (
+	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
+	github.com/Microsoft/go-winio v0.4.17 // indirect
+	github.com/Microsoft/hcsshim v0.8.23 // indirect
+	github.com/cenkalti/backoff/v4 v4.1.2 // indirect
 	github.com/chromedp/cdproto v0.0.0-20220208224320-6efb837e6bc2 // indirect
+	github.com/containerd/cgroups v1.0.1 // indirect
+	github.com/containerd/containerd v1.5.9 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/docker/docker v20.10.11+incompatible // indirect
+	github.com/docker/go-units v0.4.0 // indirect
 	github.com/elazarl/goproxy v0.0.0-20220115173737-adb46da277ac // indirect
-	github.com/fatih/color v1.7.0 // indirect
+	github.com/fatih/color v1.10.0 // indirect
 	github.com/getkin/kin-openapi v0.94.0 // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/snappy v0.0.3 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/klauspost/compress v1.13.1 // indirect
+	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/mattn/go-colorable v0.1.4 // indirect
-	github.com/mattn/go-isatty v0.0.10 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/moby/sys/mount v0.2.0 // indirect
+	github.com/moby/sys/mountinfo v0.5.0 // indirect
+	github.com/moby/term v0.0.0-20210619224110-3f7ff695adc6 // indirect
+	github.com/morikuni/aec v0.0.0-20170113033406-39771216ff4c // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.2 // indirect
+	github.com/opencontainers/runc v1.0.2 // indirect
 	github.com/pierrec/lz4/v4 v4.1.8 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.opencensus.io v0.22.4 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
@@ -50,7 +73,7 @@ require (
 	github.com/magefile/mage v1.13.0 // indirect
 	github.com/mattetti/filebuffer v1.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.10 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
 	github.com/mitchellh/go-testing-interface v1.0.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
@@ -63,7 +86,7 @@ require (
 	github.com/prometheus/procfs v0.7.3 // indirect
 	github.com/rivo/uniseg v0.1.0 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
-	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/net v0.0.0-20211108170745-6635138e15ea // indirect
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
 	golang.org/x/text v0.3.6 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
---
+++ pkg/framer/framer_test.go
@@ -0,0 +1,208 @@
+package framer_test
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"testing"
+	"time"
+
+	"github.com/docker/go-connections/nat"
+	"github.com/grafana/astradb-datasource/pkg/plugin"
+	"github.com/grafana/grafana-plugin-sdk-go/experimental"
+	"github.com/stargate/stargate-grpc-go-client/stargate/pkg/auth"
+	"github.com/stargate/stargate-grpc-go-client/stargate/pkg/client"
+	pb "github.com/stargate/stargate-grpc-go-client/stargate/pkg/proto"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+	"google.golang.org/grpc"
+)
+
+var (
+	grpcEndpoint string
+	authEndpoint string
+)
+
+const updateGoldenFile = false
+
+func init() {
+	ctx := context.Background()
+
+	astraDbContainer, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image: "stargateio/stargate-3_11:v1.0.40",
+			Env: map[string]string{
+				"CLUSTER_NAME":    "test",
+				"CLUSTER_VERSION": "3.11",
+				"DEVELOPER_MODE":  "true",
+				"ENABLE_AUTH":     "true",
+			},
+			ExposedPorts: []string{"8090/tcp", "8081/tcp", "8084/tcp", "9042/tcp"},
+			WaitingFor:   wait.ForHTTP("/checker/readiness").WithPort("8084/tcp").WithStartupTimeout(90 * time.Second),
+		},
+		Started: true,
+	})
+	if err != nil {
+		log.Fatalf("Failed to start Stargate container: %v", err)
+	}
+
+	grpcPort, err := nat.NewPort("tcp", "8090")
+	if err != nil {
+		log.Fatalf("Failed to get port: %v", err)
+	}
+
+	authPort, err := nat.NewPort("tcp", "8081")
+	if err != nil {
+		log.Fatalf("Failed to get port: %v", err)
+	}
+
+	grpcEndpoint, err = astraDbContainer.PortEndpoint(ctx, grpcPort, "")
+	if err != nil {
+		log.Fatalf("Failed to get endpoint: %v", err)
+	}
+
+	authEndpoint, err = astraDbContainer.PortEndpoint(ctx, authPort, "")
+	if err != nil {
+		log.Fatalf("Failed to get endpoint: %v", err)
+	}
+
+}
+
+func createClient(t *testing.T) *client.StargateClient {
+	conn, err := grpc.Dial(grpcEndpoint, grpc.WithInsecure(), grpc.WithBlock(),
+		grpc.WithPerRPCCredentials(
+			auth.NewTableBasedTokenProviderUnsafe(
+				fmt.Sprintf("http://%s/v1/auth", authEndpoint), "cassandra", "cassandra",
+			),
+		),
+	)
+	require.NoError(t, err)
+
+	astraDbClient, err := client.NewStargateClientWithConn(conn)
+	require.NoError(t, err)
+	return astraDbClient
+}
+
+//TODO: TestFramer currently tests for Frame() method returns correct frame
+// But various data type fields returns null instead of actual data. This needs to be fixed
+// Also later different types can be added
+func TestFramer(t *testing.T) {
+	stargateClient := createClient(t)
+
+	// create keyspace
+	query := &pb.Query{
+		Cql: "CREATE KEYSPACE IF NOT EXISTS grafana WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 1};",
+	}
+	response, err := stargateClient.ExecuteQuery(query)
+	require.NoError(t, err)
+
+	assert.Nil(t, response.GetResultSet())
+
+	// add table to keyspace
+	cql := `
+   CREATE TABLE IF NOT EXISTS grafana.tempTable1 (
+     id uuid PRIMARY KEY,
+     asciivalue ascii,
+	 textvalue text,
+	 varcharvalue varchar,
+	 blobvalue blob,
+	 booleanvalue boolean,
+	 decimalvalue decimal,
+	 doublevalue double,
+  	 floatvalue float,
+	 inetvalue inet,
+     bigintvalue bigint,
+	 intvalue int,
+     smallintvalue smallint,
+	 varintvalue varint,
+	 tinyintvalue tinyint,
+	 timevalue time,
+	 timestampvalue timestamp,
+     datevalue date,
+     timeuuidvalue timeuuid,
+     mapvalue map<int,text>,
+     listvalue list<text>,
+     setvalue set<text>,
+     tuplevalue tuple<int, text, float>
+   );`
+	query = &pb.Query{
+		Cql: cql,
+	}
+	response, err = stargateClient.ExecuteQuery(query)
+	require.NoError(t, err)
+
+	assert.Nil(t, response.GetResultSet())
+
+	// insert into table
+	cql = `
+	INSERT INTO grafana.tempTable1 (
+		id, 
+		asciivalue,
+		textvalue,
+		varcharvalue,
+		blobvalue,
+		booleanvalue,
+		decimalvalue,
+		doublevalue,
+		floatvalue,
+		inetvalue,
+		bigintvalue,
+		intvalue,
+		smallintvalue,
+		varintvalue,
+		tinyintvalue,
+		timevalue,
+		timestampvalue,
+		datevalue,
+		timeuuidvalue,
+		mapvalue,
+		listvalue,
+		setvalue,
+		tuplevalue
+	) VALUES (
+		f066f76d-5e96-4b52-8d8a-0f51387df76b,
+		'alpha', 
+		'bravo',
+		'charlie',
+		textAsBlob('foo'),
+		true,
+		1.1,
+        2.2,
+		3.3,
+		'127.0.0.1',
+        1,
+		2,
+		3,
+		4,
+		5,
+        '10:15:30.123456789',
+        '2021-09-07T16:40:31.123Z',
+        '2021-09-07',
+		30821634-13ad-11eb-adc1-0242ac120002,
+		{1: 'a', 2: 'b', 3: 'c'},
+		['a', 'b', 'c'],
+		{'a', 'b', 'c'},
+		(3, 'bar', 2.1)
+	);
+	`
+	query = &pb.Query{
+		Cql: cql,
+	}
+	response, err = stargateClient.ExecuteQuery(query)
+	require.NoError(t, err)
+
+	assert.Nil(t, response.GetResultSet())
+
+	// read from table
+	query = &pb.Query{
+		Cql: "SELECT * FROM grafana.tempTable1",
+	}
+	response, err = stargateClient.ExecuteQuery(query)
+	require.NoError(t, err)
+
+	frameResponse := plugin.Frame(response)
+	require.NotNil(t, frameResponse)
+	experimental.CheckGoldenJSONFrame(t, "testdata", "allTypes", frameResponse, updateGoldenFile)
+}
