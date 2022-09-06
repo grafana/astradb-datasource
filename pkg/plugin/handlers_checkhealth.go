3bacb295750ab59f5b06366d77d10944e5abaeb9
Scott Lepper <scott.lepper@gmail.com>
2022-09-06 17:05:56 -0400

    fix healthcheck (#31)
    fix healthcheck

--- pkg/plugin/handlers_checkhealth.go
+++ pkg/plugin/handlers_checkhealth.go
@@ -5,44 +5,40 @@ import (
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/stargate/stargate-grpc-go-client/stargate/pkg/client"
+	pb "github.com/stargate/stargate-grpc-go-client/stargate/pkg/proto"
 )
 
 func (d *AstraDatasource) CheckHealth(_ context.Context, req *backend.CheckHealthRequest) (*backend.CheckHealthResult, error) {
 
 	if d.settings.URI == "" {
-		return &backend.CheckHealthResult{
-			Status:  backend.HealthStatusError,
-			Message: "Invalid AstraDB URL",
-		}, nil
+		return newHealthResult(backend.HealthStatusError, "Invalid AstraDB URL")
 	}
 
 	if d.settings.Token == "" {
-		return &backend.CheckHealthResult{
-			Status:  backend.HealthStatusError,
-			Message: "Invalid AstraDB Token",
-		}, nil
+		return newHealthResult(backend.HealthStatusError, "Invalid AstraDB Token")
 	}
 
-	var status = backend.HealthStatusOk
-	var message = "Data source is working"
+	if err := d.connect(); err != nil {
+		return newHealthResult(backend.HealthStatusError, err.Error())
+	}
 
-	err := d.connect()
-
+	c, err := client.NewStargateClientWithConn(d.conn)
 	if err != nil {
-		status = backend.HealthStatusError
-		message = err.Error()
-	}
-	if err == nil {
-		_, err := client.NewStargateClientWithConn(d.conn)
-		if err != nil {
-			status = backend.HealthStatusError
-			message = err.Error()
-		}
+		return newHealthResult(backend.HealthStatusError, err.Error())
 	}
 
+	if _, err = c.ExecuteQuery(&pb.Query{
+		Cql: "select keyspace_name from system_schema.keyspaces;",
+	}); err != nil {
+		return newHealthResult(backend.HealthStatusError, err.Error())
+	}
+
+	return newHealthResult(backend.HealthStatusOk, "Data source is working")
+}
+
+func newHealthResult(status backend.HealthStatus, message string) (*backend.CheckHealthResult, error) {
 	return &backend.CheckHealthResult{
 		Status:  status,
 		Message: message,
 	}, nil
-
 }
