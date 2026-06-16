7130c63826b4cd8861445f08345794d67491e880
Timur Olzhabayev <timur.olzhabayev@grafana.com>
2026-06-16 10:13:44 +0200

    chore: make grafanaDependency prerelease-inclusive

---
+++ src/utils/formatSql.ts
@@ -0,0 +1,7 @@
+import { format } from 'sql-formatter';
+
+export function formatSQL(q: string) {
+  return format(q).replace(/(\$ \{ .* \})|(\$ __)|(\$ \w+)/g, (m: string) => {
+    return m.replace(/\s/g, '');
+  });
+}
